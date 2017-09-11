19ca210cf61a11860cdd570113c7a52a6fe072aa
Roberto De Ioris <roberto@20tab.com>
2017-09-11 19:34:55 +0200

    more tests

--- bvh.py
+++ bvh.py
@@ -2,67 +2,146 @@ import re
 
 class BvhNode:
 
-   def __init__(self, value=None, parent=None):
-       self.value = value
-       self.children = []
-       self.parent = parent
-       if self.parent:
-           self.parent.add_child(self)
+    def __init__(self, value=[], parent=None):
+        self.value = value
+        self.children = []
+        self.parent = parent
+        if self.parent:
+            self.parent.add_child(self)
 
-   def add_child(self, item):
-       item.parent = self
-       self.children.append(item)
+    def add_child(self, item):
+        item.parent = self
+        self.children.append(item)
 
-   def get_property_value(self, key):
-       for child in self.children:
-           if key == child.value[0]:
-               return child.value[1:]
-       return None
+    def filter(self, key):
+        for child in self.children:
+            if child.value[0] == key:
+                yield child
 
-   def filter(self, key):
-       for child in self.children:
-           if child.value[0] == key:
-               yield child
+    def __iter__(self):
+        for child in self.children:
+            yield child
 
-   def __iter__(self):
-       for child in self.children:
-           yield child
+    def __getitem__(self, key):
+        for child in self.children:
+            for index, item in enumerate(child.value):
+                if item == key:
+                    if index+1 >= len(child.value):
+                        return None
+                    else:
+                        return child.value[index+1:]
+        raise IndexError('key {0} not found'.format(key))
 
-   def __repr__(self):
-       return str(' '.join(self.value))
+    def __repr__(self):
+        return str(' '.join(self.value))
 
 
 class Bvh:
 
-   def __init__(self, data):
-       self.data = data
-       self.root = BvhNode()
-       self.frames = []
-       self.tokenize()
+    def __init__(self, data):
+        self.data = data
+        self.root = BvhNode()
+        self.frames = []
+        self.tokenize()
 
-   def tokenize(self):
-       first_round = []
-       accumulator = ''
-       for char in self.data:
-           if char not in ('\n', '\r'):
-               accumulator += char
-           else:
-               if accumulator:
-                   first_round.append(re.split('\\s+', accumulator.strip()))
-                   accumulator = ''
-       node_stack = [self.root]
-       frame_time_found = False
-       for item in first_round:
-           if frame_time_found:
-               self.frames.append(item)
-               continue
-           key = item[0]
-           if key == '{':
-               node_stack.append(node)
-           elif key == '}':
-               node_stack.pop()
-           else:
-               node = BvhNode(item)
-               node_stack[-1].add_child(node)
-           if item[0] == 'Frame' and item[1] == 'Time:':
-               frame_time_found = True
+    def tokenize(self):
+        first_round = []
+        accumulator = ''
+        for char in self.data:
+            if char not in ('\n', '\r'):
+                accumulator += char
+            else:
+                if accumulator:
+                    first_round.append(re.split('\\s+', accumulator.strip()))
+                    accumulator = ''
+        node_stack = [self.root]
+        frame_time_found = False
+        for item in first_round:
+            if frame_time_found:
+                self.frames.append(item)
+                continue
+            key = item[0]
+            if key == '{':
+                node_stack.append(node)
+            elif key == '}':
+                node_stack.pop()
+            else:
+                node = BvhNode(item)
+                node_stack[-1].add_child(node)
+            if item[0] == 'Frame' and item[1] == 'Time:':
+                frame_time_found = True
+
+    def search(self, *items):
+        found_nodes = []
+        def check_children(node):
+            if len(node.value) >= len(items):
+                failed = False
+                for index, item in enumerate(items): 
+                    if node.value[index] != item:
+                        failed = True
+                        break
+                if not failed:
+                    found_nodes.append(node)
+            for child in node:
+                check_children(child)
+        check_children(self.root)
+        return found_nodes
+
+    def get_joints(self):
+        joints = []
+        def iterate_joints(joint):
+            joints.append(joint)
+            for child in joint.filter('JOINT'):
+                iterate_joints(child)
+        iterate_joints(next(self.root.filter('ROOT')))
+        return joints
+
+    def get_joint_index(self, name):
+        return self.get_joints().index(self.get_joint(name))
+
+    def get_joint(self, name):
+        found = self.search('ROOT', name)
+        if not found:
+            found = self.search('JOINT', name)
+        if found:
+            return found[0]
+        raise LookupError('joint not found')
+
+    def joint_offset(self, name):
+        joint = self.get_joint(name)
+        offset = joint['OFFSET']
+        return (float(offset[0]), float(offset[1]), float(offset[2]))
+
+    def joint_channels(self, name):
+        joint = self.get_joint(name)
+        return joint['CHANNELS'][1:]
+
+    def get_joint_channels_index(self, joint_name):
+        index = 0
+        for joint in self.get_joints():
+            if joint.value[1] == joint_name:
+                return index 
+            index += int(joint['CHANNELS'][0])
+        raise LookupError('joint not found')
+
+    def frame_joint_channel(self, frame_index, joint, channel, default_value=None):
+        joint_index = self.get_joint_channels_index(joint)
+        channel_index = self.joint_channels(joint).index(channel)  
+        if channel_index == -1 and default_value is not None:
+            return default_value
+        return float(self.frames[frame_index][joint_index + channel_index])
+        
+    @property
+    def nframes(self):
+        try:
+            return int(next(self.root.filter('Frames:')).value[1])
+        except StopIteration:
+            raise LookupError('number of frames not found')
+
+    @property
+    def frame_time(self):
+        try:
+            return float(next(self.root.filter('Frame')).value[2])
+        except StopIteration:
+            raise LookupError('frame time not found')
+        
--- tests/test_bvh.py
+++ tests/test_bvh.py
@@ -4,13 +4,6 @@ from bvh import Bvh, BvhNode
 
 class TestBvh(unittest.TestCase):
 
-    """
-    def test_get_format_need_more(self):
-        mp3 = Mpg123()
-        with self.assertRaises(Mpg123.NeedMoreException):
-            mp3.get_format()
-    """
-
     def test_file_read(self):
         with open('tests/test_freebvh.bvh') as f:
             mocap = Bvh(f.read())
@@ -48,7 +41,97 @@ class TestBvh(unittest.TestCase):
         self.assertEqual(bones[17], 'JOINT mixamorig:LeftHandThumb2')
         self.assertEqual(bones[22], 'JOINT mixamorig:LeftHandRing1')
         self.assertEqual(bones[30], 'JOINT mixamorig:RightForeArm')
-        
+
+    def test_offset(self):
+        with open('tests/test_mocapbank.bvh') as f:
+            mocap = Bvh(f.read())
+        self.assertEqual(next(mocap.root.filter('ROOT'))['OFFSET'], ['0.0000', '0.0000', '0.0000'])
+
+    def test_search(self):
+        with open('tests/test_mocapbank.bvh') as f:
+            mocap = Bvh(f.read())
+        self.assertEqual([str(node) for node in mocap.search('JOINT', 'LeftShoulder')], ['JOINT LeftShoulder'])
+
+    def test_search_single_item(self):
+        with open('tests/test_mocapbank.bvh') as f:
+            mocap = Bvh(f.read())
+        self.assertEqual([str(node) for node in mocap.search('ROOT')], ['ROOT Hips'])
+
+    def test_search_single_item_joints(self):
+        with open('tests/test_mocapbank.bvh') as f:
+            mocap = Bvh(f.read())
+        self.assertEqual(len(mocap.search('JOINT')), 18)
+
+    def test_joint_offset(self):
+        with open('tests/test_mocapbank.bvh') as f:
+            mocap = Bvh(f.read())
+        self.assertEqual(mocap.joint_offset('RightElbow'), (-2.6865, -25.0857, 1.2959))
+
+    def test_unknown_joint(self):
+        with open('tests/test_mocapbank.bvh') as f:
+            mocap = Bvh(f.read())
+        with self.assertRaises(LookupError):
+            mocap.joint_offset('FooBar')
+
+    def test_unknown_attribute(self):
+        with open('tests/test_freebvh.bvh') as f:
+            mocap = Bvh(f.read())
+        with self.assertRaises(IndexError):
+            mocap.root['Broken']
+
+    def test_nframes_red_light(self):
+        mocap = Bvh('')
+        with self.assertRaises(LookupError):
+            mocap.nframes
+
+    def test_nframes(self):
+        with open('tests/test_freebvh.bvh') as f:
+            mocap = Bvh(f.read())
+        self.assertEqual(mocap.nframes, 69)
+
+    def test_frame_time(self):
+        with open('tests/test_freebvh.bvh') as f:
+            mocap = Bvh(f.read())
+        self.assertEqual(mocap.frame_time, 0.0333333)
+
+    def test_nframes2(self):
+        with open('tests/test_mocapbank.bvh') as f:
+            mocap = Bvh(f.read())
+        self.assertEqual(mocap.nframes, 455)
+
+    def test_nframes_with_frames_list(self):
+        with open('tests/test_mocapbank.bvh') as f:
+            mocap = Bvh(f.read())
+        self.assertEqual(mocap.nframes, len(mocap.frames))
+
+    def test_channels(self):
+        with open('tests/test_mocapbank.bvh') as f:
+            mocap = Bvh(f.read())
+        self.assertEqual(mocap.joint_channels('LeftElbow'), ['Zrotation', 'Xrotation', 'Yrotation'])
+        self.assertEqual(mocap.joint_channels('Hips'), ['Xposition', 'Yposition', 'Zposition', 'Zrotation', 'Xrotation', 'Yrotation'])
+
+    def test_frame_channel(self):
+        with open('tests/test_mocapbank.bvh') as f:
+            mocap = Bvh(f.read())
+        self.assertEqual(mocap.frame_joint_channel(22, 'Hips', 'Xrotation'), -20.98)
+        self.assertEqual(mocap.frame_joint_channel(22, 'Chest', 'Xrotation'), 17.65)
+        self.assertEqual(mocap.frame_joint_channel(22, 'Neck', 'Xrotation'), -6.77)
+        self.assertEqual(mocap.frame_joint_channel(22, 'Head', 'Yrotation'), 8.47)
+
+    def test_frame_channel(self):
+        with open('tests/test_freebvh.bvh') as f:
+            mocap = Bvh(f.read())
+        self.assertEqual(mocap.frame_joint_channel(22, 'mixamorig:Hips', 'Xposition'), 4.3314)
+
+    def test_frame_iteration(self):
+        with open('tests/test_mocapbank.bvh') as f:
+            mocap = Bvh(f.read())
+        x_accumulator = 0.0
+        for i in range(0, mocap.nframes):
+            x_accumulator += mocap.frame_joint_channel(i, 'Hips', 'Xposition') 
+        self.assertTrue(abs(-19735.902699999995 - x_accumulator) < 0.0001)
+    
+
 
 if __name__ == '__main__':
     unittest.main()
